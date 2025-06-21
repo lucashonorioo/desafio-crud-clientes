e92cbb9abeea5fdd328fcb2bc96da2c50ba70571
Lucas Honorio <lucashonorioo@hotmail.com>
2025-06-21 17:38:12 -0300

    Projeto Criado

---
+++ src/main/java/com/estudo/desafio_crud_clientes/DesafioCrudDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.desafio_crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudDeClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/desafio_crud_clientes/DesafioCrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.desafio_crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
