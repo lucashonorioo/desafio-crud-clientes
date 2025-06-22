0dcd91f834262294c57e6c0cfa142272cdcc700a
Lucas Honorio <lucashonorioo@hotmail.com>
2025-06-22 16:40:11 -0300

    Implementando Client Service

--- src/main/java/com/estudo/desafio_crud_clientes/service/ClientService.java
+++ src/main/java/com/estudo/desafio_crud_clientes/service/ClientService.java
@@ -1,4 +1,15 @@
 package com.estudo.desafio_crud_clientes.service;
 
+import com.estudo.desafio_crud_clientes.dto.request.ClientRequestDTO;
+import com.estudo.desafio_crud_clientes.dto.response.ClientResponseDTO;
+
+import java.util.List;
+
 public interface ClientService {
+
+    ClientResponseDTO findByIdClient(Long id);
+    List<ClientResponseDTO> findAllClients();
+    ClientResponseDTO insertClient(ClientRequestDTO clientRequestDTO);
+    ClientResponseDTO updateClient(Long id, ClientRequestDTO clientRequestDTO);
+    void deleteClient(Long id);
 }
--- src/main/java/com/estudo/desafio_crud_clientes/service/impl/ClientServiceImpl.java
+++ src/main/java/com/estudo/desafio_crud_clientes/service/impl/ClientServiceImpl.java
@@ -1,4 +1,46 @@
 package com.estudo.desafio_crud_clientes.service.impl;
 
-public class ClientServiceImpl {
+import com.estudo.desafio_crud_clientes.dto.request.ClientRequestDTO;
+import com.estudo.desafio_crud_clientes.dto.response.ClientResponseDTO;
+import com.estudo.desafio_crud_clientes.repository.ClientRepository;
+import com.estudo.desafio_crud_clientes.service.ClientService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+
+    @Override
+    public ClientResponseDTO findByIdClient(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<ClientResponseDTO> findAllClients() {
+        return List.of();
+    }
+
+    @Override
+    public ClientResponseDTO insertClient(ClientRequestDTO clientRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public ClientResponseDTO updateClient(Long id, ClientRequestDTO clientRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteClient(Long id) {
+
+    }
+    
 }
