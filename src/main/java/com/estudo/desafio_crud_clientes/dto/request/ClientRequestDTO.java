44560128f2d9fc5c6180e1eba6e5088f6e942363
Lucas Honorio <lucashonorioo@hotmail.com>
2025-06-22 16:06:58 -0300

    Implementando classes Client dto, request e response

--- src/main/java/com/estudo/desafio_crud_clientes/dto/request/ClientRequestDTO.java
+++ src/main/java/com/estudo/desafio_crud_clientes/dto/request/ClientRequestDTO.java
@@ -1,4 +1,50 @@
 package com.estudo.desafio_crud_clientes.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
 public class ClientRequestDTO {
+    @NotBlank(message = "O campo nome não pode ser vazio")
+    private String name;
+
+    private String cpf;
+    private Double income;
+
+    @PastOrPresent(message = "A data não pode ser futura")
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientRequestDTO(){
+
+    }
+
+    public ClientRequestDTO(String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
 }
--- src/main/java/com/estudo/desafio_crud_clientes/dto/response/ClientResponseDTO.java
+++ src/main/java/com/estudo/desafio_crud_clientes/dto/response/ClientResponseDTO.java
@@ -1,4 +1,50 @@
 package com.estudo.desafio_crud_clientes.dto.response;
 
+import java.time.LocalDate;
+
 public class ClientResponseDTO {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientResponseDTO(){
+
+    }
+
+    public ClientResponseDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
 }
